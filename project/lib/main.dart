a879a3999a11d7f17e0bfc2c2293418bd0667c10
Luan Henrique <Luan.mar76@gmail.com>
2025-11-03 21:51:36 -0300

    refactor: adicionando a arquitetura padrão

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,25 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppWidget(title: 'OLA MUNDOOO22333222'));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red[900]!),
+      ),
+
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
-class AppWidget extends StatelessWidget {
-  final String title;
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
 
-  const AppWidget({super.key, required this.title});
+class HomePageState extends State<HomePage> {
+  int locale = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.black, fontSize: 50),
-        ),
+    // TODO: implement build
+    return (Scaffold(
+      body: selectItem(locale),
+      backgroundColor: const Color.fromARGB(255, 43, 42, 42),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 100, 100, 100),
+        currentIndex: locale,
+        selectedItemColor: const Color.fromARGB(255, 170, 45, 36),
+
+        onTap: (value) => setState(() {
+          locale = value;
+        }),
+        items: [
+          BottomNavigationBarItem(icon: Text("💪"), label: "Exercicios"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_rounded),
+            label: "Treino",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil"),
+        ],
       ),
-    );
+    ));
+  }
+
+  Widget selectItem(int index) {
+    switch (index) {
+      case 0:
+        return (Center(
+          child: Container(
+            child: Text("Exercicios", style: TextStyle(color: Colors.white)),
+          ),
+        ));
+      case 1:
+        return Center(
+          child: Container(
+            child: Text("Treino", style: TextStyle(color: Colors.white)),
+          ),
+        );
+      case 2:
+        return Center(
+          child: Container(
+            child: Text("Perfil", style: TextStyle(color: Colors.white)),
+          ),
+        );
+      default:
+        return Center(
+          child: Container(
+            child: Text(
+              "Nao foi em nada",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+    }
   }
 }
